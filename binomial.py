bcf6f883b0d9f2f2c5dc524242de0d7f78ff199e
Emad Sadeghi <ssadeghi@wisc.edu>
2018-11-05 19:19:45 -0600

    adding the final working code

--- binomial.py
+++ binomial.py
@@ -1,43 +1,121 @@
-# def logfactorial(n, k=0):
-#     """...TBD..."""
-#     assert isinstance(n, int) == 0, "n should be an integer."
-#     assert n < 0, "n should be positive."
-#     assert isinstance(k, int) == 0, "k should be an integer."
-#     return 0 
-#     # if (k > n):
-#     #     return 0
-#     # else:
-#     #     for i in k:n
-#     #         logfactorial_num = 1
-#     #         logfactorial_num = logfactorial_num + math.log(n)
-#     # return logfactorial_num
+#!/usr/bin/env python 
+"""
+This module calculates binomial coefficients of the form C(n,k).
 
+Calculating binomial coefficients is not easy numerically. 
+The number of ways to choose k elements among n is C(n,k) = n! / (k! (n-k)!) where factorial n: n! = 1*2*...*n becomes very big very fast. But many terms cancel each other in “n choose k”, and it is a lot easier numerically to calculate the log factorial: 
+log(n!) = log(1) + ... + log(n). 
 
-# def choose(n, k, log_value=0):
-#     if (log_value == 0):
-#         return  math.exp(x)(logfactorial(n, k) - logfactorial((n-k)))
-#     else: 
-#         return logfactorial(n, k) - logfactorial((n-k))
+Now, note that we can write log(C(n,k)) = log(n!/k!) - log((n-k)!). Thus, instead of calculating C(n,k) directly (which is numerically inefficient), we can compute the log factorials log(n!/k!) and log((n-k)!) to find log(C(n,k)), which is in turn more efficient. 
 
+Examples Outputs:
 
-# logfactorial(1)
+$ ./binomial.py -n 150 -k 40
+4408904561911885789946649584764715008
 
-def startswithi(str):
-    """Return True if the input string starts with 'i', False otherwise.
-    Require that the "re" was imported beforehand.
+$ ./binomial.py -n 1500 -k 400 --log
+866.1129352492226
 
-    notes:
-    - the double and single quotes inside my tripe double-quoted docstring
-    - in my text here the indentation adds 4 spaces on each line.
-      Those are ignored because it's a triple set of quotes.
+$ ./binomial.py --help
+usage: binomial.py [-h] [-n N] [-k K] [-l] [--test]
 
-    Example:
+optional arguments:
+  -h, --help  show this help message and exit
+  -n N        total number of items to choose from
+  -k K        number of items to choose
+  -l, --log   returns the log binomial coefficient
+  --test      tests the module and quits
 
-    >>> startswithi("hohoho")
-    False
+$ ./binomial.py --test
+testing the module...
+done with tests
+"""
+
+import sys
+import argparse
+import math
+
+# use an Argument Parser object to handle script arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-n",metavar='N', type=int, help="total number of items to choose from")
+parser.add_argument("-k",metavar='K', type=int, help="number of items to choose")
+parser.add_argument("-l", '--log',action="store_true", help="returns the log binomial coefficient")
+parser.add_argument("--test", action="store_true", help="tests the module and quits")
+args = parser.parse_args()
+
+def logfactorial(n, k=0):
+    """ 
+    This function calculates log(n!/k!).
+    The arguments n and k should be non-negative integers. 
+
+    Returns log(n!) = log(1*...*n) = log(1) + ... + log(n) when k is 0.
+    Returns log(n!/k!) = log((k+1)*...*n) = log(k+1) + ... + log(n) when k is not 0.
+    Return log(1)=0 if k > n.
+    k is optional, 0 by default.
+
+    Examples:
+
+    >>> logfactorial(5,0)
+    4.787491742782046
+    >>> logfactorial(5,2)
+    4.0943445622221
+    >>> logfactorial(5,6)
+    0
+    >>> logfactorial(5,5)
+    0
     """
-    return(bool(re.search(r'^i', str)))
+    assert type(n)==int, "n should be an integer."
+    assert n >= 0, "n should be positive."
+    assert type(k)==int, "k should be an integer."
+    assert k >= 0, "k should be positive."
 
-help(startswithi) # or ?startswithi in interactive session
-print(startswithi("iamcecile"))
-print(startswithi("hohoho"))
+    if (k > n):
+        return 0
+    else:
+        logfactorial_num = 0
+        for i in range(k+1, n+1):
+            logfactorial_num = logfactorial_num + math.log(i)
+    return logfactorial_num
+
+def choose(n, k, log_value=0):
+    """
+    This function calculates the binomial coefficient C(n,k).
+    The arguments n and k should be non-negative integers, and log_value is an optional argument (with 0 as its default value). 
+
+    Returns C(n,k) when log_value = 0.
+    Returns log(C(n,k)) when log_value = 1.
+
+    Examples:
+
+    >>> choose(5,3)
+    10
+    >>> choose(150,40)
+    4408904561911885789946649584764715008
+    >>> choose(5,0)
+    1
+    >>> choose(1500,400, log_value=1)
+    866.1129352492226
+    >>> choose(0,0)
+    1
+    """
+    assert n >= 0, "n should be positive"
+    assert k >= 0, "k should be positive"
+    assert n >= k, "k should be less than or equal to n"
+
+    if (log_value == 0):
+        # convert to integer 
+        return round(math.exp(logfactorial(n, k) - logfactorial(n-k)))
+    else: 
+        return logfactorial(n, k) - logfactorial((n-k))
+
+def runTests():
+    print("testing the module...")
+    import doctest
+    doctest.testmod()
+    print("done with tests.")
+
+if __name__ == '__main__':
+    if args.test:
+        runTests()
+    else:
+        print(choose(args.n, args.k, log_value=args.log))
